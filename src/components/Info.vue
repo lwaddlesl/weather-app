ba1934001ad9ec839f53dcbedae10c58bc2f7a43
Nikita <nekit.zernov@yandex.ru>
2022-02-02 01:40:01 +0300

    fix bugs

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,13 +1,17 @@
 <template>
-  <main class="info">
-    <div class="name">
-      {{ $store.state.weather.name }}
-    </div>
-    <div class="temp">
-      Temp: {{ Math.round($store.state.weather.main.temp) }}
-    </div>
-    <div class="temp">
-      feels like: {{ Math.round($store.state.weather.main.feels_like) }}
+  <main>
+    <div class="info" v-if="$store.state.weather.main">
+      <div class="name">
+        {{ $store.state.weather.sys.country }},{{ $store.state.weather.name }}
+      </div>
+
+      <div class="temp">{{ Math.round($store.state.weather.main.temp) }}°C</div>
+      <div class="feels-like">
+        {{ Math.round($store.state.weather.main.feels_like) }}°C
+      </div>
+      <div class="state">
+        {{ $store.state.weather.weather[0].main }}
+      </div>
     </div>
   </main>
 </template>
@@ -18,9 +22,25 @@ export default {};
 
 <style scoped>
 * {
-  color: black;
+  color: white;
+}
+.info {
+  margin-top: 10px;
 }
 .name {
   font-size: 7vw;
+}
+.temp {
+  font-size: 5vw;
+  font-style: italic;
+  background-color: rgba(255, 255, 255, 0.25);
+}
+.feels-like {
+  font-size: 5vw;
+  font-style: italic;
+  background-color: rgba(255, 255, 255, 0.25);
+}
+.state {
+  font-size: 5vw;
 }
 </style>
